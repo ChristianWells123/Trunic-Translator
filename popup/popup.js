cb64991c343505937170f1c06105f030c707c998
Christian Wells <Christian.Wells@ttu.edu>
2022-04-09 11:20:09 -0500

    styled popup window and added icons

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,21 @@
 async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab.id;
-  }
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.id;
+}
 
 async function translatePage() {
-    tabId = await getCurrentTab();
-    console.log("executing on id " + tabId);
-    chrome.scripting.executeScript({
-      target: {tabId: tabId},
-      files: ['main.js']
-    }); 
-  }
-  
-  document.getElementById('translate-button').addEventListener('click', () => translatePage(), false);
+  tabId = await getCurrentTab();
+  console.log("executing on id " + tabId);
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    files: ['main.js']
+  });
+}
+
+let fontUrl = chrome.runtime.getURL('resources/Trunic.woff');
+
+let font = new FontFace("Trunic", "url('" + fontUrl + "')");
+document.fonts.add(font);
+
+document.getElementById('translate-button').addEventListener('click', () => translatePage(), false);
